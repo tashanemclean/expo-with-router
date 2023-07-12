8024fcc36deedb18071510138608d100aafa22fa
Tashane Mclean <48889185+tashanemclean@users.noreply.github.com>
2023-07-12 02:46:37 -0400

    build success

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,9 @@
-module.exports = function (api) {
+module.exports = (api) => {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
+    presets: ['babel-preset-expo'],
     plugins: [
-      "@babel/plugin-proposal-export-namespace-from",
-      "react-native-reanimated/plugin",
-      require.resolve("expo-router/babel"),
+      require.resolve('expo-router/babel'),
     ],
   };
 };
